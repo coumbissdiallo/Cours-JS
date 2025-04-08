33345b4c540bfe3d059a10489bf500012b541e92
unknown <diallocoumbiss676@gmail.com>
2025-04-08 11:53:03 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,141 @@
+// let a=parseInt(prompt ("entrer le premier nombre"))
+// let operation=prompt("veillez saisir le signe (+,-,*,/,%) :")
+// let b=parseInt(prompt("veillez saisir le deuxiéme nombre"))
+
+// function add(a,b) {
+//     let c=a+b
+//     console.log(c);
+// }
+// function sous(a,b) {
+//     let c=a-b
+//     console.log(c);
+// }
+// function mult(a,b) {
+//     let c=a*b
+//     console.log(c);
+// }
+// function div(a,b) {  
+//     if (a<=0) {
+//         console.log("veillez saisir un nombre supérieur à 0");
+//     }
+//          else if (b<=0) {
+//              console.log("veillez saisir un nombre supérieur à 0");
+//          }   
+//          else{
+//         let c=a/b
+//         console.log(c);
+//     }  
+    
+// }
+// function mod(a,b) {
+//     let c=a%b
+//     console.log(c);
+// }
+// function calculatrice(a,b,operation) {
+//     if (operation==="+") {
+//         add(a,b); 
+//     }
+   
+    
+//     else if (operation==="-") {
+//         sous(a,b); 
+//      }
+     
+     
+//      else if (operation==="*") {
+//         mult(a,b); 
+//      }
+  
+     
+//      else if (operation==="/") {
+//         div(a,b); 
+//      }
+     
+     
+//       else if (operation==="%") {
+//         mod(a,b); 
+//      }
+//      else{
+//     console.log("operation non reconnue");
+//     }
+    
+    
+// }
+// calculatrice(a,b,operation)
+
+
+        // function direBonjour() {
+        //     console.log("Bonjour");
+        // }
+        // for (let i = 0; i <=6; i++) {
+        //     console.log(`voilà le premier tour ${i}`);
+            
+        // }
+
+
+        //le while s'exécute seulement quand la condition rsete vrai sinon elle deveint fausse ;elle s'arrete
+        // let nom="coumba";
+        // let i=0
+        // while (i<7) {
+        //     console.log((i+1) + "." +nom);
+        //     i++;
+        // }
+    
+
+        //le do while s'éxécute au moins une fois meme si la condition est fausse avant de vérifier si la condition est vrai
+        // let i=0;
+        // do {
+        //     console.log((i+1) + "." +nom);
+        //     i++;
+        // } while (i<7);
+
+
+
+        //ls tableaux
+        // let nom=[
+        //     "Thiam",
+        //     "Sow",
+        //     "Sy",
+        //     "Dia",
+        // ];
+        // nom[4]=("Ba");
+        // console.log(nom);
+
+        // let nom=[
+        //     "Thiam",
+        //     "Sow",
+        //     "Sy",
+        //     "Dia",
+        // ];
+        // nom.push=("Ba");
+        // console.log(nom);
+
+        // let nom=[
+        //     "Thiam",
+        //     "Sow",
+        //     "Sy",
+        //     "Dia",
+        // ];
+        // nom [4]=("Ba");
+        // nom.shift();
+        // console.log(nom);
+         
+
+
+        // let nom=[
+        //     "Thiam",
+        //     "Sow",
+        //     "Sy",
+        //     "Dia",
+        // ];
+        // nom [4] =("Ba");
+        // nom [5] =("Ndao");
+        // console.log(nom);
+
+
+
+     
+
+
+        
+
